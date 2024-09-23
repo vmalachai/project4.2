3e3fc8ef554ced88624f6919a6902dce2eb31af5
Malachai Vargas <mvarg712stu.southtexascollege.edu>
2024-09-23 00:21:20 -0500

    totals added

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,29 @@
 <script setup>
 import Header from './components/Head.vue';
+import Totals from './components/Totals.vue';
+
+const recipes = ref([]);
+
+const totalCost = computed(() => {
+    return recipes.value.reduce((acc, recipe) => {
+        return acc + recipe.totalCost;
+    }, 0);
+});
+
+const totalCalories = computed(() => {
+    return recipes.value.reduce((acc, recipe) => {
+        return acc + recipe.totalCalories;
+    }, 0);
+});
+
 
 </script>
 
 <template>
+  <Header></Header>
+<div class="container">
 
-<Header></Header>
+<Totals :totalCost="totalCost" :totalCalories="totalCalories"></Totals>
 
+</div>
 </template>
---
+++ src/components/Totals.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+    totalCost: Number,
+    totalCalories: Number
+});
+</script>
+
+<template>
+    <div class="totals">
+        <h2>Totals</h2>
+        <p>Total Cost: ${{ totalCost.toFixed(2) }}</p>
+        <p>Total Calories: {{ totalCalories }}</p>
+    </div>
+</template>
